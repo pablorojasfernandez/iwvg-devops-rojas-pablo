b4cc1d0fd00ed75498ededf79f316562aa922f4a
pablorojasfernandez <pablo.rojas.fernandez@alumnos.upm.es>
2025-09-23 14:26:40 +0200

    Add FractionTest class #7

--- src/test/java/es/upm/miw/devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/devops/code/FractionTest.java
@@ -47,6 +47,77 @@ class FractionTest {
     }
 
     @Test
+    void testIsProper() {
+        Fraction proper = new Fraction(2, 5);
+        Fraction improper = new Fraction(7, 3);
+
+        assertThat(proper.isProper()).isTrue();
+        assertThat(improper.isProper()).isFalse();
+    }
+
+    @Test
+    void testIsImproper() {
+        Fraction proper = new Fraction(2, 5);
+        Fraction improper = new Fraction(7, 3);
+
+        assertThat(proper.isImproper()).isFalse();
+        assertThat(improper.isImproper()).isTrue();
+    }
+
+    @Test
+    void testIsEquivalent() {
+        Fraction f1 = new Fraction(1, 2);
+        Fraction f2 = new Fraction(2, 4);
+        Fraction f3 = new Fraction(3, 5);
+
+        assertThat(f1.isEquivalent(f2)).isTrue();
+        assertThat(f1.isEquivalent(f3)).isFalse();
+    }
+
+    @Test
+    void testAdd() {
+        Fraction f1 = new Fraction(1, 2);
+        Fraction f2 = new Fraction(1, 3);
+
+        Fraction result = f1.add(f2);
+
+        assertThat(result.getNumerator()).isEqualTo(5);
+        assertThat(result.getDenominator()).isEqualTo(6);
+    }
+
+    @Test
+    void testMultiply() {
+        Fraction f1 = new Fraction(2, 3);
+        Fraction f2 = new Fraction(3, 4);
+
+        Fraction result = f1.multiply(f2);
+
+        assertThat(result.getNumerator()).isEqualTo(6);
+        assertThat(result.getDenominator()).isEqualTo(12);
+    }
+
+    @Test
+    void testDivide() {
+        Fraction f1 = new Fraction(2, 3);
+        Fraction f2 = new Fraction(3, 4);
+
+        Fraction result = f1.divide(f2);
+
+        assertThat(result.getNumerator()).isEqualTo(8);
+        assertThat(result.getDenominator()).isEqualTo(9);
+    }
+
+    @Test
+    void testDivideByZeroNumeratorFractionThrows() {
+        Fraction f1 = new Fraction(1, 2);
+        Fraction f2 = new Fraction(0, 5);
+
+        assertThatThrownBy(() -> f1.divide(f2))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("Cannot divide by a fraction with numerator 0");
+    }
+
+    @Test
     void testToString() {
         Fraction fraction = new Fraction(5, 6);
 
